4b9830e618c02f7e2b555731ea184c7bc87ad74c
Wilmer Huaqui Huayllacayan <whuaquih@emeal.nttdata.com>
2024-04-19 15:11:54 -0500

    reports

---
+++ src/main/java/com/bootcamp51/microservices/reportsservice/ReportsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp51.microservices.reportsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp51/microservices/reportsservice/ReportsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp51.microservices.reportsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
